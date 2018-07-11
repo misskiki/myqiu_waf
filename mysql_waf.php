96bc53531caa222a0e648481f345905ab4134266
misskiki <41108094+misskiki@users.noreply.github.com>
2018-07-11 18:06:19 +0800

    mysql_waf.php
    出于爱好写了这个waf脚本，暂且功能就只有这些后期慢慢加把。
    用法 require_once('myqiu_waf.php'); 
    放在数据库配置文件里面就好了

--- mysql_waf.php
+++ mysql_waf.php
@@ -1,14 +1,23 @@
 <?php
+/*
+myqiu_waf.php 1.0
+www.myqiu.org
+*/
 $getfilter="select|(and|or)\\b.+?(>|<|=|in|like)|\\/\\*.+?\\*\\/|<\\s*script\\b|\\bEXEC\\b|UNION.+?SELECT|UPDATE.+?SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE).+?FROM|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)";
 $postfilter="\\b(and|or)\\b.{1,6}?(=|>|<|\\bin\\b|\\blike\\b)|\\/\\*.+?\\*\\/|<\\s*script\\b|\\bEXEC\\b|UNION.+?SELECT|UPDATE.+?SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE).+?FROM|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)";
 $cookiefilter="\\b(and|or)\\b.{1,6}?(=|>|<|\\bin\\b|\\blike\\b)|\\/\\*.+?\\*\\/|<\\s*script\\b|\\bEXEC\\b|UNION.+?SELECT|UPDATE.+?SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE).+?FROM|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)";
 function Codepage($strgets,$strgetsq,$strgetsql){
 	if(preg_match("/".$strgetsql."/is",$strgetsq)==1){
-	$url=$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'];
-        
+	$url='http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'];
 	echo '<br>You have triggered the WAF protection rules';
         echo '<br>你已经触发waf防护规则';
         echo '<br>提交地址危险地址'.$url;
+        $sqlfile='log.txt';
+        $content='$url\r\n';
+        if($sqlfile_url=file_put_contents($sqlfile,$content,FILE_APPEND)){
+        echo '你的操作记录已经被记录';
+
+}
         //echo '<script>window.close();</script>';
 	exit();	
 }
